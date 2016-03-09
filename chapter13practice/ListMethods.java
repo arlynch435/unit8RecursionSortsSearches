9de61c354da57f3e5cf301aa7e315f1be1136224
arlynch435 <ARLynch@student.naperville203.org>
2016-03-09 15:08:12 -0600

    started recursion programs

---
+++ chapter13practice/ListMethods.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class ListMethods
+{
+   public static ArrayList<Integer> makeList(int n)
+   {
+      ArrayList<Integer> tempList = null;
+      if (n <= 0)  // The smallest list we can make
+      {
+          tempList=new ArrayList<Integer>(0);
+      }
+      else        // All other size lists are created here
+      {
+          tempList=makeList(n-1);
+          tempList.add(n);
+      }
+      return tempList;
+   }
+   public static ArrayList<Integer> deepClone(ArrayList<Integer> tList)
+    {
+       ArrayList<Integer> list = new ArrayList<Integer>(); 
+       for (Integer i : tList)
+       {
+          list.add(new Integer(i));
+       }
+       return list;
+    }
+   public static ArrayList<Integer> reverseList(ArrayList<Integer> tList)
+    {
+       ArrayList<Integer> list = ListMethods.deepClone(tList); 
+       if (list.size()<=1)// The list is empty or has one element
+       {
+          return list;
+       }
+       else
+       {
+          // Use the solution to a smaller version of the problem
+          int n= list.get(0);
+          list.remove(0);
+          list=reverseList(list);
+          list.add(n);
+          // to solve the general problem
+       }
+       return list;
+    }
+   public static ArrayList even(ArrayList<Integer> tList)
+   {
+       ArrayList<Integer> list = ListMethods.deepClone(tList);
+       if (list.size()<=2)
+       {
+           int n=list.get(1);
+           list= new ArrayList<Integer>(0);
+           list.add(n);
+        }
+        else
+        {
+        }
+        return list;
+    }
+    public static void main(String[] args)
+   {
+      ArrayList<Integer> tempList = ListMethods.makeList(100);
+      if (tempList.size() == 0)
+      {
+          System.out.println("The list is empty");
+      }
+      else
+      {
+         for (Integer i : tempList)
+         {
+            System.out.println(i);
+         }
+      }
+      tempList=ListMethods.reverseList(tempList);
+      if (tempList.size() == 0)
+      {
+          System.out.println("The list is empty");
+      }
+      else
+      {
+         for (Integer i : tempList)
+         {
+            System.out.println(i);
+         }
+      }
+   }
+}
